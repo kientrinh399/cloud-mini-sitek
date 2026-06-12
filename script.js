c447d232f8d8fd870aac57849ea5d7fa3d33a298
kientrinh399 <155545142+kientrinh399@users.noreply.github.com>
2026-06-12 09:58:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function tảiApi() {
     try {
         // Dán cái link mày vừa copy vào giữa hai dấu nháy kép này:
-        const response = await fetch(https://cloud-mini-sitek.kient7665.workers.dev);
+        const response = await fetch(https://cloud-mini-sitek.kient7665.workers.dev/);
         
         const data = await response.json();
         document.getElementById("kết quả").innerText = JSON.stringify(data, null, 2);
